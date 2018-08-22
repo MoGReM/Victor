ae725ccfaac8588a5b37643624571afc25170922
MoGReM <40706098+MoGReM@users.noreply.github.com>
2018-08-22 10:51:03 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -184,55 +184,7 @@ message.author.send(`**مدة الرابط : يـوم
 
  
 
-client.on("guildMemberAdd", (member) => {
 
-    let channel = member.guild.channels.get("474355074467102726");
-
-    if (!channel) {
-
-        console.log("!the channel id it's not correct");
-
-        return;
-
-    }
-
-    if (member.id == client.user.id) {
-
-        return;
-
-    }
-
-    console.log('-');
-
-    var guild;
-
-    while (!guild)
-
-        guild = client.guilds.get("474327533622525952");
-
-    guild.fetchInvites().then((data) => {
-
-        data.forEach((Invite, key, map) => {
-
-            var Inv = Invite.code;
-
-            if (dat[Inv])
-
-                if (dat[Inv] < Invite.uses) {
-
- channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;       
-
- }
-
-            dat[Inv] = Invite.uses;
-
-       
-
-       });
-
-    });
-
-});
 
 
 
