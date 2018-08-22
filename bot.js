35d538e5528fb9c6503a1d912e9a4156fcf67b6b
MoGReM <40706098+MoGReM@users.noreply.github.com>
2018-08-21 18:49:58 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -105,8 +105,6 @@ var now_playing = [];
 \\\\\\\\\\\\\\\\\\\\\\\\/////////////////////////
 */
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
